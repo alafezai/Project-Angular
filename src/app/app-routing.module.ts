f570c6fc8d4ab0e1525a83f6df8fe63338291df6
Ahmed Ben Houria <ahmedbenhouria1999.bha@gmail.com>
2021-11-14 22:38:52 +0100

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'HelpMe', loadChildren: () => import('./help-me/help-me.module').then(m => m.HelpMeModule) }
+  { path: 'HelpMe', loadChildren: () => import('./help-me/help-me.module').then(m => m.HelpMeModule) },
+  { path: 'SignIn' , component: SigninComponent},
+  { path: 'SignUp' , component: SignupComponent},
+
 ]
 
 @NgModule({
