6fd8f7cc7b7c707428b3a846c47b2892f3e0f79b
alafezai <fezaiala650@gmail.com>
2021-11-27 21:31:23 +0100

    create fake data with use  Post Interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { NavbarAdminComponent } from './admin/navbar-admin/navbar-admin.component';
+import { NOTFOUND } from 'dns';
 
 
 
@@ -16,8 +17,9 @@ const routes: Routes = [
   { path: 'SignIn' , component: SigninComponent},
   { path: 'SignUp' , component: SignupComponent},
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  {path : 'admin/navbar-admin' , component : NavbarAdminComponent},
+  { path : 'admin/navbar-admin' , component : NavbarAdminComponent},
   
+  //{path : 'notfound', component:},
   { path: 'module-help', loadChildren: () => import('./module-help/module-help.module').then(m => m.ModuleHelpModule) },
   
 
---
+++ src/app/help-me/posts-list.ts
@@ -0,0 +1,14 @@
+import { Post } from "./post.modele";
+
+let posts: Post[] = [
+{
+    id: '8f4aa769-7841-424c-a817-a6b50ae75c66',
+    title: 'Introduction to Computer Science',
+},
+{
+    id: '8f4aa769-7841-424c-a817-a6b50ae75c66',
+    title: 'Introduction to Computer Science',
+}
+];
+
+export default posts;
