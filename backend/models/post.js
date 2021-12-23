078acc94dbcefd9883c45b414ea00e48154a8cfa
alafezai <fezaiala650@gmail.com>
2021-12-23 22:59:05 +0100

    use post modele to retrive postes list

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const { type } = require('os');
 const postSchema  = new mongoose.Schema({
-    user : {type :array ,require: true},
-    sujet : {type : string, required : true },
-    datePost : {type : string ,required : true},
-    nbComent : {type : number, required : true},
-    comments : {type : array , required : ture},
-    nbJaime : {type : number , required : true}
+    user : {type :Array ,require: true},
+    sujet : {type : String, required : true },
+    datePost : {type : String ,required : true},
+    nbComent : {type : Number, required : true},
+    comments : {type : Array , required : true},
+    nbJaime : {type : Number , required : true}
 });
 
 module.exports  = mongoose.model('Poste',postSchema,'postes');
