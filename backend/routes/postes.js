3822ac5844b05dbbcbfb7c626065eca674fcc444
alafezai <fezaiala650@gmail.com>
2021-12-23 23:16:47 +0100

    Find postes by id

--- backend/routes/postes.js
+++ backend/routes/postes.js
@@ -4,14 +4,28 @@ const express = require('express');
 const router = express.Router();
 
 const poste = require('./../models/Post');
-//GET ALL POSTES
+/**
+ * 
+ * 
+ * GET ALL POSTES
+ */
 router.get('/',(req,res)=>{
     poste.find()
     .then(postes=>res.status(200).json(postes))
     .catch(err=>res.status(400).json({error : 'error : '+err.message}));
-     // res.json(posts);
-     // res.send('hello word');
  });
 
+
+
+ /**
+  * 
+  * 
+  * FIND POSTE BY ID
+  */
+ router.get('/:id',(req,res)=>{
+    poste.findOne({_id : req.params.id})
+    .then(postes=>res.status(200).json(postes))
+    .catch(err=>res.status(400).json({error : 'error : '+err.message}));
+ });
 
 module.exports = router;
