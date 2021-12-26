e7bf6da1bcef310c882d46662e19aa0408f1e880
alafezai <fezaiala650@gmail.com>
2021-12-26 22:52:50 +0100

    add promostion component

--- src/app/promostion/promostion.routing.module.ts
+++ src/app/promostion/promostion.routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PromostionComponent } from "./promostion.component";
-  
+
 const routes: Routes = [
   { path: "", component: PromostionComponent },
+  { path: "TEST", component: PromostionComponent },
 
 ];
-  
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
