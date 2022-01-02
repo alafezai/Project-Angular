22da4a7c4572bb24ca3c0c23d7b239f3b04b61da
alafezai <fezaiala650@gmail.com>
2022-01-02 23:20:26 +0100

    template login and sing up

--- src/app/promostion/promostionComponent/promostion.component.ts
+++ src/app/promostion/promostionComponent/promostion.component.ts
@@ -1,3 +1,4 @@
+import { PromostionsService } from './../../services/promostions.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class PromostionComponent {
   title = 'Promostion';
+
+  public promostionList: any = [];
+  constructor(
+    private promostionService : PromostionsService,
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.promostionService.GetAllpromostions().subscribe(
+      res=>this.promostionList = res
+    );
+  }
 }
---
+++ src/app/singup/singup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.css']
+})
+export class SingUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
